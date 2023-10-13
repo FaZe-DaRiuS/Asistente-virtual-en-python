ea708e591cb2a43fb4dfddb2af8bc6a6bd1ba96e
FaZe-DaRiuS <147876276+FaZe-DaRiuS@users.noreply.github.com>
2023-10-13 21:53:32 +0200

    start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,7 @@
+@echo off
+echo Instalando las bibliotecas desde requirements.txt...
+pip install -r requirements.txt
+echo Todas las bibliotecas se han instalado correctamente.
+pause
+
 python -m alexa
